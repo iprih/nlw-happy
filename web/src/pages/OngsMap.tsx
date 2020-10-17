31ff9bdbc8d0651e1c1ffec8790a66a746d743fe
iprih <priscila.silva076@gmail.com>
2020-10-17 16:52:14 -0300

    aula 3

--- web/src/pages/OngsMap.tsx
+++ web/src/pages/OngsMap.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import {Link} from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import {Map, TileLayer} from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css';
-
-import mapMarkerImg from '../images/logo_mapa.jpg'
+import mapMarkerImg from '../images/mark2.png'
+import mapIcon from "../utils/mapIcon";
 
 import '../styles/pages/ongs-map.css'
+import api from '../services/api';
 
+// const mapIcon = Leaflet.icon({
+//     iconUrl: mapMarkerImg,
+// })
 
-
-function OngsMap() {
+interface Ong {
+    id: number;
+    name: string;
+    latitude: number;
+    longitude: number;
+  }
+  
+function OngsMap(){
+    const [ongs, setOngs] = useState<Ong[]>([]);
+  
+    useEffect(() => {
+      api.get("/ongs").then(({ data }) => {
+        setOngs(data);
+      });
+    }, []);
+    
     return (
      <div id="page-map">
         <aside>
@@ -33,9 +50,31 @@ function OngsMap() {
        >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+        {ongs.map((ong) => {
+          return (
+            <Marker
+              key={ong.id}
+              icon={mapIcon}
+              position={[ong.latitude, ong.longitude]}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className="map-popup"
+              >
+                {ong.name}
+                <Link to={`/ongs/${ong.id}`}>
+                  <FiArrowRight size="20" color="#FFF" />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
         </Map>
 
-        <Link to="" className="create-ong">
+        <Link to="/ongs/create" className="create-ong">
             <FiPlus size={32} color="#FFF"/>
 
         </Link>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OngsMap from './pages/OngsMap';
+import Ong from './pages/Ong';
+import CreateOng from './pages/CreateOng';
+
 
 function Routes() {
      return (
          <BrowserRouter>
+          <Switch>
             <Route path="/" exact component={Landing}/>
             <Route path="/app" component={OngsMap}/>
+            <Route path="/ongs/create" component={CreateOng}/>
+            <Route path="/ongs/:id" component={Ong}/>
+          </Switch>
          </BrowserRouter>
      );
 }
